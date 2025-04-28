d05f625791a1d2dadc477822aa4cdce6914ef68d
William Anderson <wanderson.wa252@gmail.com>
2025-04-28 14:10:17 -0400

    fixed structure

---
+++ pipeline/truck_loader/run_pipeline.py
@@ -0,0 +1,21 @@
+def main():
+    # Step 1: Recieve order confirmation to trigger the pipeline
+
+    # Step 2: Parse CSV into a DataFrame
+
+    # Step 3: Parse PDF into special instructions
+
+    # Step 4: Save email specific data
+
+    # Step 5: Combine data into a single object
+
+    # Step 6: Compare against master excel sheet and identify new items
+
+    # Step 7: Pass all items to the truck loader
+
+    # Step 8: Process truck loader response into something for employees
+
+    print("Pipeline completed successfully.")
+
+if __name__ == "__main__":
+    main()
